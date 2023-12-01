c3c62ef945b163c0b9a70eb622cb4104c73e9147
MasFana <adrianfathan69@gmail.com>
2023-12-01 22:02:56 +0700

    Delete unnecessary files and update dependencies

--- lara/routes/web.php
+++ lara/routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get("/admin", [App\Http\Controllers\AdminController::class, "index"])->name("admin");
